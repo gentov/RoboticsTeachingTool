dd6d8042b2f26e7ec83996528da51d6725a4a110
RichardHosea <richard.d.hosea@gmail.com>
2020-07-12 22:32:11 -0400

    I forgot to make a branch but I'm pushing what I have right now. I'm still working on the meat of it but for now the structure of the pages is there. Used Filter Module to help me out (ty gabe). I still need to put a back button on the 2nd PID page

---
+++ SimpleDrone.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class SimpleDrone():
+    def __init__(self, mass, pos):
+        self.mass = mass
+        self.pos = pos
+        self.power = 0
+        self.gravity = 9.81
+        self.gravityForce = self.mass*self.gravity
+        self.minPower = 0
+        self.maxPower = self.mass*self.gravity*2.5  # I want to limit how much power to make 2.5 times weight
+
+    def fly(self, power, t):
+        if power > self.maxPower:
+            power = self.maxPower
+        if power < self.minPower:
+            power = self.minPower
+        self.pos = self.pos + (power - self.gravityForce)*t
