857f5f69705253b754e131f049dffe8180a810aa
tberg1234 <33205989+tberg1234@users.noreply.github.com>
2020-07-25 23:10:13 -0400

    edge detection working

--- CVModule.py
+++ CVModule.py
@@ -3,14 +3,26 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from edge_detection import *
+
 class CVModule(Module):
     def __init__(self, gui = None, title = None):
         super(CVModule, self).__init__(gui = gui, title = title)
         self.cv_overview = tk.PhotoImage(file = "cv_home_example.png")
         self.cv_connections = tk.PhotoImage(file = "Computer-vision-apply-for-medical-image-processing.png")
         self.edge_detect = tk.PhotoImage(file = "edge_pepper_cropped5.png")
+        self.img_for_ed_interactive = tk.PhotoImage("single_rose.png")
         # tk.Frame.__init__(self)
         # self.tk.Frame.master.bind('<Configure>', self.resize_image)
+
+        self.quizQuestionMark = tk.PhotoImage(file="quizQuestionMark.png")
+        self.quizPassedImage = tk.PhotoImage(file="passedQuiz.png")
+        self.quizFailedImage = tk.PhotoImage(file="failedQuiz.png")
+        self.radioVarCVQ1 = tk.StringVar()
+        self.radioVarCVQ2 = tk.StringVar()
+
+        self.font = ('Comic Sans MS', 11, 'bold italic')
+
 
     def introPage(self):
         self.gui.clearScreen()
@@ -51,7 +63,7 @@ class CVModule(Module):
         im_temp.save("test.png", "png")
 
         #int(300 * float(w / 2)),int(300 * float(h / 2))
-        self.visualizingPane.create_image(int(300 * float(w / 2)),int(300 * float(h / 2)), image=tk.PhotoImage(file = "test.png"), anchor=tk.CENTER)
+        self.visualizingPane.create_image(0,0, image=tk.PhotoImage(file = "test.png"), anchor=tk.CENTER)
 
         self.placeNextButton(.7, .7, pane=self.interactivePane,
                              text="Edge \n Detection", font=font, command=self.edge_detection)
@@ -71,12 +83,177 @@ class CVModule(Module):
         #                      text="Let's go!", font=font, command=self.edge_detection)
         self.placeBackButton(.1, .7, pane=self.interactivePane, command=self.connection,
                              text="CV vs ML", font=font)
+        # self.placeNextButton(.7, .7, pane=self.interactivePane,
+        #                      text="Main Menu", font=font, command=self.gui.HomePage)
+        # self.gui.moduleDict["Computer Vision"].completed = True
         self.placeNextButton(.7, .7, pane=self.interactivePane,
-                             text="Main Menu", font=font, command=self.gui.HomePage)
-        self.gui.moduleDict["Computer Vision"].completed = True
+                             text="ED Interaction", font=font, command=self.edge_detection_interactive)
+
+    def edge_detection_interactive(self):
+        self.gui.clearScreen()
+        #INSTEAD OF SELF.MAKE PANES
+        self.interactivePane = tk.Canvas(self.gui.win, width=500, height=200, bg='grey')
+        sliderPane = tk.Canvas(self.gui.win, width=500, height=200, bg='grey')
+        self.visualizingPane = tk.Canvas(self.gui.win, width=500, height=200, bg='white')
+        second_visualizingPane = tk.Canvas(self.gui.win, width=500, height=200, bg='grey')
+        self.interactivePane.grid(row=0, column=0)
+        sliderPane.grid(row=1, column=0)
+        self.visualizingPane.grid(row=0, column=1)
+        second_visualizingPane.grid(row=1, column=1)
+
+        self.visualizingPane.create_image(250, 100, image=self.img_for_ed_interactive, anchor=tk.CENTER)
+        ed = EdgeDecectionandGaussianBlur().edge_detection()
+        im = Image.fromarray(ed)
+        imgtk = ImageTk.PhotoImage(image=im)
+
+        second_visualizingPane.create_image(50, 50, image=imgtk, anchor=tk.NE)
+
+        font = ('Comic Sans MS', 11, 'bold italic')
+        self.showText(275, 150, "THIS IS SO DUMB", self.interactivePane, font)
+
+        minval_slider = tk.Scale(sliderPane, from_=0, to=200, orient=tk.HORIZONTAL)
+        maxval_slider = tk.Scale(sliderPane, from_=0, to=200, orient=tk.HORIZONTAL)
+        minval_slider.pack()
+        maxval_slider.pack()
+
+
+        # recalc = tk.Button(sliderPane, text='Recalc Image', command=self.recalc_image)
+        # recalc.pack()
+        # w = tk.Scale(self.interactivePane, from_=0, to=200, orient=HORIZONTAL)
+        # w.pack()
+
+        self.placeBackButton(.1, .7, pane=self.interactivePane, command=self.edge_detection,
+                             text="ED Activity", font=font)
+        self.placeNextButton(.7, .7, pane=self.interactivePane,
+                             text="Gaussian Filtering", font=font, command=self.gaussian)
+
+    def gaussian(self):
+        self.gui.clearScreen()
+        # INSTEAD OF SELF.MAKE PANES
+        self.interactivePane = tk.Canvas(self.gui.win, width=500, height=200, bg='grey')
+        sliderPane = tk.Canvas(self.gui.win, width=500, height=200, bg='grey')
+        self.visualizingPane = tk.Canvas(self.gui.win, width=500, height=200, bg='white')
+        second_visualizingPane = tk.Canvas(self.gui.win, width=500, height=200, bg='grey')
+        self.interactivePane.grid(row=0, column=0)
+        sliderPane.grid(row=1, column=0)
+        self.visualizingPane.grid(row=0, column=1)
+        second_visualizingPane.grid(row=1, column=1)
+
+        ed = EdgeDecectionandGaussianBlur().edge_detection()
+        im = Image.fromarray(ed)
+        imgtk = ImageTk.PhotoImage(image=im)
+
+        self.visualizingPane.create_image(250, 100, image=self.img_for_ed_interactive, anchor=tk.NE)
+        second_visualizingPane.create_image(50, 50, image=imgtk, anchor=tk.NE)
+
+        font = ('Comic Sans MS', 11, 'bold italic')
+        self.showText(275, 150, "THIS IS SO DUMB pt 2", self.interactivePane, font)
+
+        kernalX_slider = tk.Scale(sliderPane, from_=0, to=200, orient=tk.HORIZONTAL)
+        kernalY_slider = tk.Scale(sliderPane, from_=0, to=200, orient=tk.HORIZONTAL)
+        sigmaX_slider= tk.Scale(sliderPane, from_=0, to=200, orient=tk.HORIZONTAL)
+        kernalX_slider.pack()
+        kernalY_slider.pack()
+        sigmaX_slider.pack()
+
+        self.placeBackButton(.1, .7, pane=self.interactivePane, command=self.edge_detection_interactive,
+                             text="ED Activity", font=font)
+        self.placeNextButton(.7, .7, pane=self.interactivePane,
+                             text="Gaussian Filtering", font=font, command=self.CVQuiz)
+
+
+    #def recalc_image(self):
+
+    def CVQuiz(self):
+        quizPrompt = "Let's put the knowlege you learned to the test!\n" \
+                     "Here are two questions to make sure you're understanding\n" \
+                     "The material so far.\n"
+        question1 = "1) What is Hysterisis Thresholding used for?\n"
+        question2 = "2) How is the gradient used in non-maximum supression?\n"
+        self.gui.clearScreen()
+        self.makePanes()
+        self.radioVarCVQ1.set(-1) # I think this makes it so that none are selected. Nice.
+        self.radioVarCVQ2.set(-1) # I think this makes it so that none are selected. Nice.
+        self.interactivePane.create_text(260, 50, text = quizPrompt, font = self.font)
+
+        # QUESTION 1
+        self.interactivePane.create_text(200, 90, text=question1, font=self.font)
+        self.visualizingPane.create_image(250, 250, image=self.quizQuestionMark, anchor=tk.CENTER)
+        A1 = tk.Radiobutton(self.interactivePane, text="A) To lighten the image",
+                            padx=20, value = "A1", bg = "grey", variable = self.radioVarCVQ1)
+        A1.place(relx = .1, rely = .2)
+        B1 = tk.Radiobutton(self.interactivePane, text="B) To change the color intensity of the image",
+                            padx=20, value = "B1", bg="grey",variable = self.radioVarCVQ1)
+        B1.place(relx=.1, rely=.25)
+        C1 = tk.Radiobutton(self.interactivePane, text="C) To remove noise from the image",
+                            padx=20, value = "C1", bg="grey", variable = self.radioVarCVQ1)
+        C1.place(relx=.1, rely=.3)
+        D1 = tk.Radiobutton(self.interactivePane, text="D) To determine which edges are true edges",
+                            padx=20, value = "D1", bg="grey", variable = self.radioVarCVQ1)
+        D1.place(relx=.1, rely=.35)
+
+        # QUESTION 2
+        self.interactivePane.create_text(250, 235, text=question2, font=self.font)
+        A2 = tk.Radiobutton(self.interactivePane, text="A) It is used to determine if a point should be put to zero or not",
+                            padx=20, value="A2", bg="grey", variable=self.radioVarCVQ2)
+        A2.place(relx=.1, rely=.525)
+        B2 = tk.Radiobutton(self.interactivePane, text="B) It is used to smooth out the image",
+                            padx=20, value="B2", bg="grey", variable=self.radioVarCVQ2)
+        B2.place(relx=.1, rely=.575)
+        C2 = tk.Radiobutton(self.interactivePane, text="C) It is used to determine where an pixel is in the image",
+                            padx=20, value="C2", bg="grey", variable=self.radioVarCVQ2)
+        C2.place(relx=.1, rely=.625)
+        D2 = tk.Radiobutton(self.interactivePane, text="D) It is used to calculate minval",
+                            padx=20, value="D2", bg="grey", variable=self.radioVarCVQ2)
+        D2.place(relx=.1, rely=.675)
+        correctAnswers = [[self.radioVarCVQ1, "D1"],[self.radioVarCVQ2, "A2"]]
+        self.placeBackToMenuButton(self.visualizingPane)
+        self.placeNextButton(.675, .75, pane=self.interactivePane,
+                             text="Submit Quiz", font=self.font, command=lambda: self.checkTest(correctAnswers))
+        self.placeBackButton(.075, .75, pane=self.interactivePane, command=self.gaussian,
+                             text="", font=self.font)
+        pass
+
+
+    # Pass the list containing the correct answer objects (multiple choice)
+    def checkTest(self, correctAnswers):
+        for answer in correctAnswers:
+            if (answer[0].get() == answer[1]):
+                pass
+            else:
+                print("Quiz Failed.")
+                self.quizFailed(self.CVQuiz)
+                return
+        print("Quiz Passed.")
+        #self.quizPassed(self.introToKalmanFilter)
+        pass
+
+    def quizPassed(self, nextPage):
+        self.gui.clearScreen()
+        canvas = tk.Canvas(self.gui.win, width=1000, height=500, bg='grey')
+        canvas.grid(row=0, column=0)
+        canvas.create_image(500, 250, image=self.quizPassedImage, anchor=tk.CENTER)
+        canvas.create_text(500, 50, text="Quiz Passed!", font=self.font)
+        self.placeBackToMenuButton(canvas)
+        self.placeNextButton(.7, .7, pane=canvas, command=nextPage, text="Next Topic!",
+                             font=self.font)
+
+    def quizFailed(self, previousPage):
+        self.gui.clearScreen()
+        canvas = tk.Canvas(self.gui.win, width=1000, height=500, bg='grey')
+        canvas.grid(row=0, column=0)
+        canvas.create_image(500, 250, image=self.quizFailedImage, anchor=tk.CENTER)
+        canvas.create_text(500, 50, text="Quiz Failed!", font=self.font)
+        self.placeBackToMenuButton(canvas)
+        self.placeBackButton(.1, .7, pane=canvas, command=previousPage, text="To Quiz",
+                             font=self.font)
+
 
     def runModule(self):
         self.gui.clearScreen()
         self.makePanes()
         self.introPage()
+
+
+        #MAKE EDGE DETECTION CHANGE PARAMETER EXAMPLE OPENCV AND PYTHON REPROCESS IMAGE
 
---
+++ edge_detection.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+class EdgeDecectionandGaussianBlur():
+
+    def __init__(self, minval=0, maxval=0, kernalX=5, kernalY=5, sigmaX=0):
+        self.minval = minval
+        self.maxval = maxval
+        self.kernalX = kernalX
+        self.kernalY = kernalY
+        self.sigmaX = sigmaX
+
+
+    def edge_detection(self):
+        img = cv2.imread('single_rose.jpg',0)
+        edges = cv2.Canny(img,self.minval,self.maxval)
+        return edges
+        #
+        # plt.subplot(121),plt.imshow(img,cmap = 'gray')
+        # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        # plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+        # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+        # plt.show()
+
+    def gaussian_blur(self):
+        #sigmaY is the same as sigmaX
+        img = cv2.imread('single_rose.jpg', 0)
+        blur = cv2.GaussianBlur(img, (self.kernalX, self.kernalY), self.sigmaX)
+        return blur
+
+        # plt.subplot(121), plt.imshow(img, cmap='gray')
+        # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        # plt.subplot(122), plt.imshow(blur, cmap='gray')
+        # plt.title('Blur Image'), plt.xticks([]), plt.yticks([])
+        # plt.show()
+
